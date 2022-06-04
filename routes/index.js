cf1c74b3ce1d4744cbdb1958bea9404aa2c30b0f
Abasibiangake <48968451+Abasibiangake@users.noreply.github.com>
2022-06-04 18:29:16 +0100

    updates
    generate the new site structure with express generator and update new files

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,30 @@
-const express = require('express');
-const router = express.Router();
-// const app = express();
-// const port = 3000;
-// app.set('view engine', 'ejs')
+var express = require('express');
+var router = express.Router();
 
-//routes
+/* GET home page. */
 router.get('/', homePage);
 router.get('/about', aboutPage);
 router.get('/project', projectsPage); 
 router.get('/service', servicePage);
 router.get('/contact', contactPage);
-// app.get('/', homePage);
-// app.get('/about', aboutPage);
-// app.get('/project', projectsPage); 
-// app.get('/service', servicePage);
-
-// app.get('/contact', contactPage);
 
 //controller
 function homePage (req, res, next) {
-  res.render('index', {title: "AJ Portfolio HomePage"});
-}
-
-function aboutPage (req, res, next) {
-    res.render('about', {title: "AJ Portfolio AboutPage"});
-}
-
-function projectsPage (req, res, next) {
-    res.render('project', {title: "AJ Portfolio Project Page"});
-}
-function servicePage (req, res, next) {
-    res.render('service', {title: "AJ Portfolio Services Page"});
-}
-function contactPage (req, res, next) {
-    res.render('contact', {title:"AJ Portfolio Contact Me Page"});
-}
-
-//set the port and runs the app
-// app.listen(port, () => {
-//     console.log(`App listening at port ${port}`)
-// })
-
+    res.render('index', {title: "AJ Portfolio HomePage"});
+  }
+  
+  function aboutPage (req, res, next) {
+      res.render('about', {title: "AJ Portfolio AboutPage"});
+  }
+  
+  function projectsPage (req, res, next) {
+      res.render('project', {title: "AJ Portfolio Project Page"});
+  }
+  function servicePage (req, res, next) {
+      res.render('service', {title: "AJ Portfolio Services Page"});
+  }
+  function contactPage (req, res, next) {
+      res.render('contact', {title:"AJ Portfolio Contact Me Page"});
+  }
 
 module.exports = router;
